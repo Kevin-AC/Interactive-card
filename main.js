4fe47e838325e3936dc71632976274e0923a1dd6
Kevin-AC <kevin.argumedo@gmail.com>
2023-01-19 19:57:20 -0500

    parte1 js

--- main.js
+++ main.js
@@ -14,19 +14,22 @@ const cvc=document.getElementById('cvc');
 const btnConf=document.getElementById('btnConfirm');
 const btnConti=document.getElementById('btnContinue');
 
-const errorNumber=document.getElementById('errorNumber')
-const montError=document.getElementById('monthError')
-const cvError=document.getElementById('cvError')
-let letter = /[a-zA-Z]/;
+const errorNumber=document.getElementById('errorNumber');
+const montError=document.getElementById('monthError');
+const cvError=document.getElementById('cvError');
 
+const tanks=document.getElementById('Thanks');
+const form=document.getElementById('form');
+
+let letter = /[a-zA-Z]/;
+let flag = false;
 nameUser.addEventListener('input',()=>{
     cardName.innerText=`${nameUser.value}`
 })
 
 number.addEventListener('input',()=>{
     let inputValue = number.value;
-    let newString = inputValue.replace(/(.{4})/g, "$1 ");
-    inputValue = newString;
+    inputValue= inputValue.replace(/(.{4})/g, "$1 ");
     if(inputValue.match(letter)){
         number.classList.add('inputError')
         errorNumber.innerText=`Wrong format,numbers only`
@@ -36,9 +39,20 @@ number.addEventListener('input',()=>{
     }
     cardNumber.innerText=`${inputValue}`
 })
+
+
+
 month.addEventListener('input',()=>{
     let monthValue=month.value
     cardMonth.innerText=monthValue
+    if(monthValue==''){
+        month.classList.add('inputError')
+        montError.classList.add('errorTxt')
+        montError.innerText=`Can't be blank`
+    }else{
+        month.classList.remove('inputError')
+        montError.innerText=``
+    }
     
 })
 year.addEventListener('input',()=>{
@@ -48,8 +62,19 @@ year.addEventListener('input',()=>{
 cvc.addEventListener('input',()=>{
     let cvValue=cvc.value
     cardCv.innerText=cvValue
+    if(cvValue==''){
+        cvc.classList.add('inputError')
+        cvError.classList.add('errorTxt')
+        cvError.innerText=`Can't be blank`
+    }else{
+        cvc.classList.remove('inputError')
+        cvError.innerText=` `
+    }
     
 })
+
+
+
 
 function error(){
    
